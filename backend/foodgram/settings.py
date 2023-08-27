633d3296c43c53f1ac291954fbfde0d3a8ddc645
Ragnar Lodbrok <ragnarlodbrok@MacBook-Air-Ragnar.local>
2023-08-28 00:16:26 +0500

    1version.test.5

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -7,7 +7,8 @@ SECRET_KEY = '6+*)i_7i&raxzp@v7=5-8eiax^nl&6%h*&4)k)&5ne2vh5zrqu'
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'damenedfinal.ddns.net', '158.160.65.235']
+ALLOWED_HOSTS = os.environ.get(
+    'ALLOWED_HOSTS', default='localhost').split(', ')
 
 INSTALLED_APPS = [
     'api.apps.ApiConfig',
