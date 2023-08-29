c9a372116358dedba3e69b015613352786fda0a9
Ragnar Lodbrok <ragnarlodbrok@MacBook-Air-Ragnar.local>
2023-08-30 04:56:17 +0500

    1version.test.8

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -127,7 +127,7 @@ REST_FRAMEWORK = {
 }
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
+STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
